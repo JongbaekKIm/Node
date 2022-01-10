811620af55e265d3e778328fce18809c2f618564
jongbaekKim <skyjbkim88@gmail.com>
2022-01-10 12:22:01 +0900

    디렉터리, 파일 이름 구별

--- Ex0110/test2.js
+++ Ex0110/test2.js
@@ -15,3 +15,21 @@ console.log("시스템의 CPU정보 \n");
 console.dir(os.cpus());
 console.log("시스템의 네트워크 인터페이스 정보\n");
 console.dir(os.networkInterfaces());
+
+var path = require("path");
+//디렉터리 이름 합치기
+var directories = ["users", "mike", "docs"];
+var docsDirectory = directories.join(path.sep);
+console.log("문서 디렉터리 : %s", docsDirectory);
+
+//디렉터리 이름과 파일 이름 합치기
+var curPath = path.join("/Users/mike", "notepad.exe");
+console.log("파일 패스 : %s", curPath);
+
+
+//path에서 디렉터리, 파일이름, 확장자 구별하기
+var filename = "c:\\Users\\mike\\notepad.exe";
+var dirname = path.dirname(filename);
+var basename = path.basename(filename);
+var extname = path.extname(filename);
+console.log("디렉터리 : %s, 파일이름 : %s, 확장자 : %s", dirname, basename, extname);
