5184e983fec08f961237156a436ef1ccfd9d90e7
jongbaekKim <skyjbkim88@gmail.com>
2022-01-17 11:27:20 +0900

    server.io를 이용한 채팅프로그램 구현 (서버연결, 전체채팅, 일대일 채팅)

--- myNodeServer/moduleServer.js
+++ myNodeServer/moduleServer.js
@@ -8,6 +8,11 @@ let bodyParser = require('body-parser')
 let cookieParser = require('cookie-parser')
 let static = require('serve-static');
 
+//soket.io모듈 불러오기
+var socketio = require('socket.io');
+//cors 사용 - 클라이언트 ajax로 요청하면 cors지원
+var cors = require('cors');
+
 // 오류 핸들러 모듈 사용
 let expressErrorHandler = require('express-error-handler');
 //Session 미들웨어 불러오기
@@ -23,10 +28,11 @@ let mongoose = require('mongoose');
 let database; let MemberSchema; let MemberModel;
 //데이터베이스에 연결
 
+
 //기본 속성 설정
 app.set('port', process.env.PORT || 3000);
 //익스프레스에서 뷰 엔진을 ejs로 설정
-app.set('/views', __dirname +"/views");
+app.set('/views', __dirname + "/views");
 app.set('view engine', 'ejs');
 //body-parser를 이용해 application/x-www-form-urlencoded 파싱
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,6 +42,58 @@ app.use(bodyParser.json());
 app.use('/public', static(path.join(__dirname, 'public')));
 app.use('/semantic', static(path.join(__dirname, 'semantic')));
 app.use('/images', static(path.join(__dirname, 'images')));
+//cors를 미들웨어로 사용하도록 등록
+app.use(cors());
+//시작된 서버 객체를 반환
+var server = app.listen(app.get('port'), function () {
+    console.log('서버가 시작됨 포트 : ' + app.get('port'))
+    connectDB();
+})
+//socket.io 서버를 시작
+var io = socketio(server);
+var login_userIds = {};
+console.log('socket.io 요청 대기중');
+
+//클라이언트가 연결했을 때의 이벤트 처리
+io.sockets.on('connection', function (socket) {
+    console.log('connection info : ', socket.request.connection._peername);
+    //소켓 객체에 클라이언트 HOST, Port 정보 속성으로 추가
+    socket.remoteAddress = socket.request.connection._peername.address;
+    socket.remotePort = socket.request.connection._peername.port;
+    //'message'이벤트를 받았을 때의 처리
+    socket.on('message', function (message) {
+        console.log('message 이벤트를 받았음.')
+        console.dir(message)
+        if (message.recepient == 'ALL') {
+            //나를 포함한 모든 클라이언트에게 메시지 전달
+            console.dir(" 나 포함 모든 클라이언트에게 message 이벤트를 전송")
+            io.sockets.emit('message', message);
+        }else{
+            if(login_userIds[message.recepient]){
+                io.sockets.to(login_userIds[message.recepient]).emit('message', message);
+                //message 이벤트를 받았을 때 일대일 채팅인 경우 상대방 소켓을 찾아 메세지 전송
+                sendResponse(socket, 'messate', '200', '메시지를 전송완료');
+            }else{
+                sendResponse(socket, 'login', '404', '상대방의 로그인 ID를 찾을 수 없음.')
+            }
+        }
+    })
+    socket.on("login", function (login) {//'login' 이벤트를 받았을 때의 처리
+        console.log('login 이벤트 발생.');
+        console.dir(login);
+        //기존 클라이언트 ID 가 없으면 클라이언트 ID를 맵에 추가
+        login_userIds[login.userId] = socket.id; //socket.id는 고유 속성이므로 변경하지 말 것
+        socket.login_userId = login.userId;
+        console.log('접속한 클라이언트 ID개수 : %d', Object.keys(login_userIds).length);
+        sendResponse(socket, "login", "200", '로그인 되었음'); //dmaekq aptlwl wjsthd
+    })
+    //응담 메시지 전송 메소드
+    function sendResponse(socket, command, code, message) {
+        var statusObj = { command: command, code: code, message: message };
+        socket.emit('response', statusObj);
+    }
+});
+
 
 
 function createMemberSchema(database) {
@@ -50,7 +108,7 @@ function connectDB() {
     //데이터베이스 연결 정보
     let databaseUrl = "mongodb://localhost:27017/bitDB";
     //데이터베이스 연결
-    console.log('데이테베이스 연결을 시도합니다.');
+    console.log('데이터베이스 연결을 시도합니다.');
     mongoose.Promise = global.Promise; // mongoose의 Promise 객체는 global의 Promise객체 사용하도록 함
     mongoose.connect(databaseUrl);
     database = mongoose.connection
@@ -79,8 +137,8 @@ router.route('/process/updateMember').post(member.procUpdateMember);
 
 app.use("/", router);
 
-app.listen(app.get('port'), function () {
-    console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
-    //데이터베이스 연결을 위한 함수호출
-    connectDB();
-})
+// app.listen(app.get('port'), function () {
+//     console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+//     //데이터베이스 연결을 위한 함수호출
+//     connectDB();
+// })
